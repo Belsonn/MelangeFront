b7b7e482ef7da8151f2d4787c7f3463aab5273cb
Krystian <ksiedlarski@gmail.com>
2020-10-22 22:08:31 +0200

    Dodanie zawartosci do komponentu oraz sciezek do routingu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,48 @@
+import { AuthGuard } from './authentication/auth.guard';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './authentication/signup/signup.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { HomeComponent } from './mainpage/home/home.component';
+import { LandingPageComponent } from './mainpage/landing-page/landing-page.component';
+import { MelangeCreateComponent } from './mainpage/melange-create/melange-create.component';
+import { MelangeViewComponent } from './mainpage/melange-view/melange-view.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingPageComponent,
+    canActivateChild: [AuthGuard],
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {
+        path: 'create',
+        component: MelangeCreateComponent,
+      },
+      {
+        path:'melange/:id',
+        component: MelangeViewComponent,
+      }
+
+    ]
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
--- src/app/mainpage/melange-view/melange-view.component.ts
+++ src/app/mainpage/melange-view/melange-view.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Melange } from 'src/app/models/melange.model';
+import { MelangeService } from '../melange.service';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-melange-view',
   templateUrl: './melange-view.component.html',
   styleUrls: ['./melange-view.component.scss']
 })
 export class MelangeViewComponent implements OnInit {
-
-  constructor() { }
+  id: string;
+  isLoading = false;
+  melange: Melange;
+  constructor(
+    private melangeService: MelangeService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.isLoading = true;
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.melangeService.getMelange(this.id).subscribe(res => {
+      this.melange = res.data.melange;
+      this.isLoading = false;
+    })
   }
-
 }
